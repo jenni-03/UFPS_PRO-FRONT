08066b0aa754d7cecf7a44e70bb0a809c921bd46
Luis Ascencio <luiscarlosasqu@ufps.edu.co>
2023-10-15 13:40:49 -0500

    Modificanod Link

--- src/utils/config/axios.config.js
+++ src/utils/config/axios.config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //Defautl config for axios
 export default axios.create(
     {
-        baseURL: "https://ufpspro-production.up.railway.app",
+        baseURL: "https://ufpspro-production.up.railway.app/",
         responseType: "json",
         timeout: 6000,
         withCredentials: true,
