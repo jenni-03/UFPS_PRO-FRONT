381678a988c818dccec68af4277503c9cd5744be
Luis Ascencio <luiscarlosasqu@ufps.edu.co>
2023-10-23 23:48:55 -0500

    Actualizando la ruta

--- src/utils/config/axios.config.js
+++ src/utils/config/axios.config.js
@@ -3,7 +3,8 @@ import axios from "axios";
 //Defautl config for axios
 export default axios.create(
     {
-        baseURL: "http://127.0.0.1:3500",
+        //baseURL: "http://127.0.0.1:3500",
+        baseURL: "https://cheerful-strudel-8a37c7.netlify.app/",
         responseType: "json",
         timeout: 6000,
         withCredentials: true,
