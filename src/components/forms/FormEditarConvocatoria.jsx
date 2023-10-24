b4f80fafa4542e39c47659900b59f381fca40d14
Luis Ascencio <luiscarlosasqu@ufps.edu.co>
2023-10-24 09:14:43 -0500

    Actualizando notificación de convocatoria

--- src/components/forms/FormEditarConvocatoria.jsx
+++ src/components/forms/FormEditarConvocatoria.jsx
@@ -58,7 +58,7 @@ export default function FormEditarConvocatoria() {
     })
 
     if(response.status === 200){
-        toast.success("¡La convocatoria se actualizón correctamente!")
+        toast.success("¡La convocatoria se actualizó correctamente!")
         navigate("/convocatorias")
     }
 
