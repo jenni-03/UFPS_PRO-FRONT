e0a8489a2d881bc224fa73a7633ffe39f696d1cd
Luis Ascencio <luiscarlosasqu@ufps.edu.co>
2023-12-05 19:03:46 -0500

    modificando ruta de la api

--- src/utils/config/axios.config.js
+++ src/utils/config/axios.config.js
@@ -3,8 +3,8 @@ import axios from "axios";
 //Defautl config for axios
 export default axios.create(
     {
-        baseURL: "http://127.0.0.1:3500/",
-        //baseURL: "https://ufpspro-production-e52c.up.railway.app/",
+        //baseURL: "http://127.0.0.1:3500/",
+        baseURL: "https://ufpspro-production-e52c.up.railway.app/",
         responseType: "json",
         timeout: 15000,
         withCredentials: true,
